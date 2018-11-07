9b0d9a1dd8ac23f299a0d1d4ae1b69857a593e02
Jeff Langston <jrlangston@gmail.com>
2018-11-07 16:07:55 -0700

    issue count and command palette fixes

--- src/tslint-sourcegraph.ts
+++ src/tslint-sourcegraph.ts
@@ -8,21 +8,20 @@ export function activate(): void {
     let tsLintConfig: IConfigurationFile | null = null
 
     function afterActivate(): void {
-        const settings = resolveSettings(sourcegraph.configuration.get<Settings>().value)
-
-        const langServerAddress = settings['tslint.langserver.address']
-        if (!langServerAddress) {
-            console.log('No tslint.langserver.address was set, exiting.')
-            return
-        }
-
-        const tslintConfigPath = settings['tslint.config.path']
-
         function activeEditor(): sourcegraph.CodeEditor | undefined {
             return sourcegraph.app.activeWindow ? sourcegraph.app.activeWindow.visibleViewComponents[0] : undefined
         }
 
         async function decorate(editor: sourcegraph.CodeEditor | undefined = activeEditor()): Promise<void> {
+            const settings = resolveSettings(sourcegraph.configuration.get<Settings>().value)
+
+            const langServerAddress = settings['tslint.langserver.address']
+            if (!langServerAddress) {
+                console.log('No tslint.langserver.address was set, exiting.')
+                return
+            }
+
+            const tslintConfigPath = settings['tslint.config.path']
             // TODO: Is there a better method to skip non typescript/javascript files ?
             if (!editor || !['typescript', 'javascript'].includes(editor.document.languageId)) {
                 return
@@ -52,14 +51,16 @@ export function activate(): void {
 
                 const resp = await fetch(request)
                 const lintResult = (await resp.json()) as LintResultResponse
-                const decorations = settings['tslint.decorations.showLintIssues'] ? lintToDecorations(lintResult) : [];
+                const decorations = settings['tslint.decorations.showLintIssues'] ? lintToDecorations(lintResult) : []
                 editor.setDecorations(null, decorations)
                 // Update context to display file issue counts in toolbar
                 const context: {
                     [key: string]: string | number | boolean | null
                 } = {}
                 if (lintResult) {
-                    context['tslint.issueCount'] = Array.isArray(lintResult.failures) ? lintResult.failures.length : 0;
+                    context[`tslint.issueCount`] = Array.isArray(lintResult.failures)
+                        ? lintResult.failures.length
+                        : 0
                 }
                 sourcegraph.internal.updateContext(context)
             } catch (err) {
@@ -68,6 +69,18 @@ export function activate(): void {
         }
         sourcegraph.configuration.subscribe(() => decorate())
         sourcegraph.workspace.onDidOpenTextDocument.subscribe(() => decorate())
+
+        // Handle the "Set TSlint config path" command (show the user a prompt for the path and save to settings).
+        sourcegraph.commands.registerCommand('tslint.setconfigPath', async () => {
+            let configPath = await sourcegraph.app.activeWindow!.showInputBox({
+                prompt: 'TSLint config file path',
+                value: 'tslint.json',
+            })
+            if (configPath !== undefined) {
+                configPath = configPath || undefined
+                return sourcegraph.configuration.get<Settings>().update('tslint.config.path', configPath)
+            }
+        })
     }
     setTimeout(afterActivate, 0)
 }
